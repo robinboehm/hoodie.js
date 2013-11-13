0bc2aee6be7b6e940333e1d5032d15bbd5748467
Sven Lito <me@svenlito.com>
2013-11-13 02:22:38 +0100

    remove debugger statement

--- src/hoodie/store.js
+++ src/hoodie/store.js
@@ -376,8 +376,6 @@ module.exports = function (hoodie, options) {
     return utils.inherits(promiseApi, methods);
   };
 
-  debugger
-
   // required backend methods
   // -------------------------
   if (!options.backend) {
